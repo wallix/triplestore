a62a0bf3fdbc3fccc4ab0e7277b3b890f0bfa70c
simcap <simcap@fastmail.com>
2017-08-03 14:18:22 +0200

    Stream binary decoder

--- streamcodec_test.go
+++ streamcodec_test.go
@@ -4,9 +4,69 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"sync"
 	"testing"
 )
+
+func TestStreamBinaryDecoding(t *testing.T) {
+	var tris []Triple
+	for i := 0; i < 10; i++ {
+		tris = append(tris, SubjPred(fmt.Sprint(i), "digit").IntegerLiteral(i))
+	}
+
+	t.Run("handles done signal", func(t *testing.T) {
+		var buf bytes.Buffer
+		ctx, cancel := context.WithCancel(context.Background()) // will stop the decoding
+		dec := NewBinaryStreamDecoder(ioutil.NopCloser(&buf))
+		results := dec.Decode(ctx)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for r := range results {
+				if r.Err != nil {
+					t.Fatal(r.Err)
+				}
+			}
+		}()
+		cancel()
+		wg.Wait()
+	})
+
+	t.Run("handles normal stream", func(t *testing.T) {
+		var buf bytes.Buffer
+		if err := NewBinaryEncoder(&buf).Encode(tris...); err != nil {
+			t.Fatal(err)
+		}
+
+		dec := NewBinaryStreamDecoder(ioutil.NopCloser(&buf))
+		results := dec.Decode(context.Background())
+
+		var all []Triple
+
+		for r := range results {
+			if r.Err != nil {
+				t.Fatal(r.Err)
+			}
+			all = append(all, r.Tri)
+		}
+
+		if got, want := len(all), 10; got != want {
+			t.Fatalf("got %d, want %d", got, want)
+		}
+		s := NewSource()
+		s.Add(all...)
+		snap := s.Snapshot()
+
+		for _, tri := range tris {
+			if !snap.Contains(tri) {
+				t.Fatalf("end result should contains triple %v", tri)
+			}
+		}
+	})
+}
 
 func TestStreamBinaryEncoding(t *testing.T) {
 	t.Run("handles nil stream", func(t *testing.T) {
